652cdbcf927afebb020d2e95032ecb67dcbb4b3a
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-07-14 17:36:09 -0500

    Render PDF - SuccesFully

--- lab07env/Src/Lab07Chaisa/Lab07Chaisa/views.py
+++ lab07env/Src/Lab07Chaisa/Lab07Chaisa/views.py
@@ -7,15 +7,23 @@ class GeneratePDF(View):
     def get(self, request, *args, **kwargs):
         template = get_template('invoice.html')
         context = {
-            "invoice_id":123,
-            "customer_name": "Anthony Chaisa",
-            "amount":1399.99,
-            "today":"Today", 
+            "invoice_id":7,
+            "customer_name": "Anthony Chaisa Fernandez",
+            "amount":21,
+            "today":"14/07/23", 
         }
         html = template.render(context)
         pdf = render_to_pdf('invoice.html',context)
-        return HttpResponse(pdf, content_type='application/pdf')
-    
+        if pdf:
+            response = HttpResponse(pdf, content_type='application/pdf')
+            filename = "Invoice_%s.pdf" %("12341231")
+            content = "inline; filename=%s" %(filename)
+            download = request.GET.get("download")
+            if download:
+                content = "attachment; filename=%s" %(filename)
+            response['Content-Disposition'] = content
+            return response
+        return HttpResponse("Not Found")
 #    def generate_view(self, request, *args, **kwargs):
 #        template = get_template('invoice.html')
 #        context = {
