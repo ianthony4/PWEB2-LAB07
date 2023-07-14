a24fde60bc07d44c9c296259d679d4e9e50c3379
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-07-14 10:24:25 -0500

    Many to Many RelationShip

--- lab07env/Src/Lab07Chaisa/lab07/models.py
+++ lab07env/Src/Lab07Chaisa/lab07/models.py
@@ -29,3 +29,17 @@ class FrameWork(models.Model):
     #Para Queary one to Many
     def __str__(self):
         return self.name
+    
+# Many to Many RelationShip
+
+class Movie(models.Model):
+    name = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
+
+class Character(models.Model):
+    name = models.CharField(max_length=20)
+    movies = models.ManyToManyField(Movie)
+    def __str__(self):
+        return self.name
+    
