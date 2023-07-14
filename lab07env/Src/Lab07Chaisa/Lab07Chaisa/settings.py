ba556827dd7408fea1c71658f0ee9c015c01319a
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-07-14 11:04:16 -0500

    DataBase Settings

--- lab07env/Src/Lab07Chaisa/Lab07Chaisa/settings.py
+++ lab07env/Src/Lab07Chaisa/Lab07Chaisa/settings.py
@@ -75,10 +75,22 @@ WSGI_APPLICATION = 'Lab07Chaisa.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    #Estamos usando SQLITE3 
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+    #Si queremos usar otra base de datos como POSTGREST
+    #Hicimos esto en el proyecto de TRAVELLO
+    #"default": {
+    #"ENGINE": "django.db.backends.postgresql",
+    #"NAME": "mydatabase",
+    #"USER": "mydatabaseuser",
+    #"PASSWORD": "mypassword",
+    #"HOST": "127.0.0.1",
+    #"PORT": "5432",
+    #}
+
 }
 
 
