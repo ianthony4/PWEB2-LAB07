1115f5836d2fa848a4a78e780612cda0517ef479
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-07-14 17:49:23 -0500

    Send Mail - Configure views,settings

--- lab07env/Src/Lab07Chaisa/Lab07Chaisa/settings.py
+++ lab07env/Src/Lab07Chaisa/Lab07Chaisa/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lab07',
+    'send',
 ]
 
 MIDDLEWARE = [
--- lab07env/Src/Lab07Chaisa/send/views.py
+++ lab07env/Src/Lab07Chaisa/send/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
+def index(request):
+    send_mail('Hola de Anthony Chaisa para el Laboratorio 7', 
+                'Hola, esto es un mensaje automatico de PWEB2',
+                'achaisa@unsa.edu.pe',
+                ['wosoke8405@kameili.com'],
+                fail_silently=False)
+    
+    return render('send/index.html')
