b62ad161bb1f3c1cad57c4c626ae5eb535c4da05
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-07-14 23:43:12 -0500

    PassWord - Removed (for Security)

--- lab07env/Src/Lab07Chaisa/Lab07Chaisa/settings.py
+++ lab07env/Src/Lab07Chaisa/Lab07Chaisa/settings.py
@@ -143,6 +143,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'anthonyleo2001@gmail.com'
+#PassWord Cambiada
 EMAIL_HOST_PASSWORD = 'vmaubnbiptinzayy'
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'anthonyleo2001@gmail.com'
